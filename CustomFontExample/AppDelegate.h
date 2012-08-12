3e3f20e0b5f6e439ef2ae41da0b5c80131c0eabc
Anthony Perozzo <aperozzo@gmail.com>
2012-08-12 11:43:48 +0200

    Initial Commit

---
+++ CustomFontExample/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  CustomFontExample
+//
+//  Created by Anthony Perozzo on 12/08/12.
+//  Copyright (c) 2012 Gilthonwe Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ CustomFontExample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CustomFontExample
+//
+//  Created by Anthony Perozzo on 12/08/12.
+//  Copyright (c) 2012 Gilthonwe Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
