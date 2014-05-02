5b7eb63e04ae8e7c496701b4449d08dfe54667dd
ChristianLazatin <c.lazatin@klab.com>
2014-05-02 16:05:24 +0800

    Chapter1

---
+++ Chapter1/Quiz/BNRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BNRAppDelegate.h
+//  Quiz
+//
+//  Created by Christian Ramir Lazain on 4/28/14.
+//  Copyright (c) 2014 klab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
