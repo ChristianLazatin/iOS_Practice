8c12a6f0e96e3c94387022e3903a6611c8aff0ce
ChristianLazatin <c.lazatin@klab.com>
2014-05-02 16:11:37 +0800

    Chapter 4 and 5 without Challenges

---
+++ Chapter4/Hypnosister/BNRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BNRAppDelegate.h
+//  Hypnosister
+//
+//  Created by Christian Ramir Lazain on 4/30/14.
+//  Copyright (c) 2014 klab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
