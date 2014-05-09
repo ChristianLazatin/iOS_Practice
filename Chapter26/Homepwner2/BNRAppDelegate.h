d3fb388f4a4675fb5a992afbe3343cf4faf2b7d7
ChristianLazatin <c.lazatin@klab.com>
2014-05-09 11:45:54 +0800

    Chapter 26

---
+++ Chapter26/Homepwner2/BNRAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  BNRAppDelegate.m
+//  Homepwner
+//
+//  Created by Christian Ramir Lazain on 5/2/14.
+//  Copyright (c) 2014 klab. All rights reserved.
+//
+#import <UIKit/UIKit.h>
+
+extern NSString * const BNRNextItemValuePrefsKey;
+extern NSString * const BNRNextItemNamePrefsKey;
+
+@interface BNRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Chapter26/Homepwner2/BNRItemsViewController.h
@@ -0,0 +1,13 @@
+//
+//  BNRAppDelegate.m
+//  Homepwner
+//
+//  Created by Christian Ramir Lazain on 5/2/14.
+//  Copyright (c) 2014 klab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRItemsViewController : UITableViewController <UIViewControllerRestoration>
+
+@end
