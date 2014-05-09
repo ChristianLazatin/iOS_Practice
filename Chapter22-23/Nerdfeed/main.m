eaa38d60898e54940b49f21fc4591b467b1a7b87
ChristianLazatin <c.lazatin@klab.com>
2014-05-09 08:33:37 +0800

    Chapter 22-23(NerdFeed) without challenges

---
+++ Chapter22-23/Nerdfeed/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Nerdfeed
+//
+//  Created by Christian Ramir Lazain on 5/7/14.
+//  Copyright (c) 2014 klab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BNRAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BNRAppDelegate class]));
+    }
+}
