de4e5551cbdf92123f7dc498ca2243bea50dfc21
ChristianLazatin <c.lazatin@klab.com>
2014-05-14 17:18:42 +0800

    Chapter 10: Bronze and silver challenges

---
+++ Chap10_challenge/Chap10_challenge/BNRDetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  BNRDetailViewController.h
+//  Chap08_Challenges
+//
+//  Created by Christian Ramir Lazain on 5/13/14.
+//  Copyright (c) 2014 klab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class BNRItem;
+
+@interface BNRDetailViewController : UIViewController
+
+@property (nonatomic, strong) BNRItem *item;
+
+@end
